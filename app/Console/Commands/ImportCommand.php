26547b0ac3bee63b94b7869b3eacc8a4155ed89d
sidoniav <98320589+sidoniav@users.noreply.github.com>
2022-11-05 19:36:15 +0000

    Initial

---
+++ app/Console/Commands/ImportCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Player;
+use App\Models\Team;
+use App\Services\PlayersService;
+use Illuminate\Console\Command;
+
+class ImportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:teams';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import players into teams from json';
+
+    /**
+     * @return void
+     */
+    public function handle()
+    {
+        $path = storage_path()."/app/public/players.json";
+        $teams = PlayersService::setTeams(json_decode(file_get_contents($path), true));
+
+        foreach ($teams as $key => $players) {
+            $team = Team::create(['name' => $key]);
+            foreach ($players as $player) {
+                Player::create([
+                    'team_id' => $team->id,
+                    'name'    => $player['name'],
+                    'score'   => $player['score'],
+                ]);
+            }
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\PlayerController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/players/index', [PlayerController::class, 'index']);
+Route::get('/api/players/teams/{id}', [PlayerController::class, 'api']);
+Route::patch('/players/switch/{playerA}/{playerB}', [PlayerController::class, 'switch']);
+Route::get('/games/index', [GameController::class, 'index']);
